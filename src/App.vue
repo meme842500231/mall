f901edc49585dd8ec4fd5707df68410268f99853
meme <842500231@qq.com>
2022-07-08 14:33:09 +0800

    学习cors跨域

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template lang="">
+  <div>
+    
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios'
+// import jsonp from 'jsonp'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  // comments:{
+  //   axios
+  // },
+  data(){
+    return{
+      age:30
+    }
+  },
+  mounted(){
+    let url ="https://www.imooc.com/activity/servicetime"
+    axios.get(url).then(()=>{
+    })
+    // jsonp(url,()=>{
+
+    // })
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="">
+  
 </style>
